ccbca21677525b50cceb8b00b4c5654829833f58
HamedAzaddana <fk71jake@gmail.com>
2025-05-16 18:39:29 +0330

    removed ModelFactory codes from index.php (not used)

--- public/index.php
+++ public/index.php
@@ -2,14 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use App\Factories\ModelFactory;
 use App\HQ\Router;
-use App\HQ\DependencyInjector;
-use App\Models\Post;
-use App\Models\User;
 
 session_start();
-
-$factory = new ModelFactory();
-$router = new Router($factory); 
+$router = new Router(); 
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
